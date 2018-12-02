d9f604479ea48fd4f75a9e7967c756ff0f315ac7
Amedee Van Gasse <amedee-github@amedee.be>
2018-12-02 18:06:14 +0100

    Print the input

---
+++ src/test/groovy/be/amedee/adventofcode/aoc2015/day01/Day01.groovy
@@ -0,0 +1,15 @@
+package be.amedee.adventofcode.aoc2015.day01
+
+import spock.lang.Specification
+
+class Day01 extends Specification {
+
+    String input = getClass().getResource("input").getText("UTF-8")
+
+    def "Day 1 - Part 1"() {
+        println(input)
+        expect:
+        true
+
+    }
+}
