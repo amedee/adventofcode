3b22d08c9458a204c8bf5ad94e0978ea9a49a66d
Amedee Van Gasse ✨🌈🦄 <amedee-github@amedee.be>
2023-12-11 22:42:09 +0100

    Add day 1

---
+++ src/main/kotlin/aoc_2015/Day01.kt
@@ -0,0 +1,40 @@
+package aoc_2015
+
+fun readStringFromResource(filePath: String) = {}.javaClass.classLoader.getResource(filePath)?.readText()
+
+fun findFloorAndBasementPosition(instructions: String): Pair<Int, Int> {
+    var floor = 0
+    var basementPosition = -1
+
+    for ((index, char) in instructions.withIndex()) {
+        when (char) {
+            '(' -> floor++
+            ')' -> {
+                floor--
+                if (floor == -1 && basementPosition == -1) {
+                    basementPosition = index + 1 // Adjust index to start from 1
+                }
+            }
+        }
+    }
+
+    return Pair(floor, basementPosition)
+}
+
+fun main() {
+    // Specify the file path
+    val filePath = "aoc_2015/Day01_input"
+
+    val puzzleInput: String = readStringFromResource(filePath)!!
+
+    val (floor, basementPosition) = findFloorAndBasementPosition(puzzleInput)
+
+    println("Santa ends up on floor $floor.")
+
+    if (basementPosition != -1) {
+        println("Santa enters the basement at character position $basementPosition.")
+    } else {
+        println("Santa never enters the basement.")
+    }
+
+}
