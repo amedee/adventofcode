e02c897df0154fee3d654b4e88a326b771975ddf
Amedee Van Gasse ✨🌈🦄 <amedee-github@amedee.be>
2023-12-29 22:57:51 +0100

    Refactor 2015/02
    Make the code more testable.

--- src/main/kotlin/be/amedee/adventofcode/aoc2015/day02/Day02.kt
+++ src/main/kotlin/be/amedee/adventofcode/aoc2015/day02/Day02.kt
@@ -3,46 +3,57 @@ package be.amedee.adventofcode.aoc2015.day02
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
+fun getSurfaceArea(
+    length: Int,
+    width: Int,
+    height: Int,
+) = 2 * ((length * width) + (width * height) + (height * length))
+
+fun getSlack(
+    length: Int,
+    width: Int,
+    height: Int,
+) = minOf(length * width, width * height, height * length)
+
 fun calculateWrappingPaperOrder(presentDimensions: List<Triple<Int, Int, Int>>): Int {
     var totalSquareFeet = 0
 
-    for ((l, w, h) in presentDimensions) {
-        val surfaceArea = 2 * (l * w + w * h + h * l)
-        val slack = minOf(l * w, w * h, h * l)
+    presentDimensions.forEach { (l, w, h) ->
+        val surfaceArea = getSurfaceArea(l, w, h)
+        val slack = getSlack(l, w, h)
         totalSquareFeet += surfaceArea + slack
     }
 
     return totalSquareFeet
 }
 
-fun readPresentDimensionsFromFile(fileName: String): List<Triple<Int, Int, Int>> {
-    val inputStream = {}.javaClass.classLoader.getResourceAsStream(fileName)
-    val presentDimensions = mutableListOf<Triple<Int, Int, Int>>()
+class Day02 {
+    fun readPresentDimensionsFromFile(fileName: String): List<Triple<Int, Int, Int>> {
+        val packageName = javaClass.`package`.name.replace(".", "/")
+        val filePath = "/$packageName/$fileName"
+        val inputStream = javaClass.getResourceAsStream(filePath)
 
-    if (inputStream != null) {
-        BufferedReader(InputStreamReader(inputStream)).useLines { lines ->
-            lines.forEach { line ->
-                val dimensions = line.split("x").map { it.toInt() }
-                if (dimensions.size == 3) {
-                    presentDimensions.add(Triple(dimensions[0], dimensions[1], dimensions[2]))
+        val presentDimensions = mutableListOf<Triple<Int, Int, Int>>()
+
+        if (inputStream != null) {
+            BufferedReader(InputStreamReader(inputStream)).useLines { lines ->
+                lines.forEach { line ->
+                    val dimensions = line.split("x").map { it.toInt() }
+                    if (dimensions.size == 3) {
+                        presentDimensions.add(Triple(dimensions[0], dimensions[1], dimensions[2]))
+                    }
                 }
             }
         }
-    }
 
-    return presentDimensions
+        return presentDimensions
+    }
 }
 
 fun main() {
-    // Specify the file path
-    val filePath = "be/amedee/adventofcode/aoc2015/day02/input"
+    val inputFile = "input"
+    val presentDimensions = Day02().readPresentDimensionsFromFile(inputFile)
 
-    // Read present dimensions from the file
-    val presentDimensions = readPresentDimensionsFromFile(filePath)
-
-    // Calculate the total square feet of wrapping paper needed
     val totalSquareFeet = calculateWrappingPaperOrder(presentDimensions)
-
-    // Print the result
     println("Elves should order $totalSquareFeet square feet of wrapping paper.")
 }
