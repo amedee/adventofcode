dd924b3f92ddbac10f8a255647503e85000a64f3
Amedee Van Gasse <amedee-github@amedee.be>
2023-12-29 04:53:21 +0100

    Solve the second puzzle of 2015/day01

--- src/test/kotlin/be/amedee/adventofcode/aoc2015/day01/Day01Test.kt
+++ src/test/kotlin/be/amedee/adventofcode/aoc2015/day01/Day01Test.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.TestInstance
  */
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class Day01Test {
-    private val input = Day01Test::class.java.getResource("input")?.readText()
+    private val input = Day01().readFile()
 
     private val oneMoveTestData =
         listOf(
@@ -49,15 +49,50 @@ class Day01Test {
             input to 280,
         )
 
+    private val findBasementTestData = listOf(
+        "" to 0,
+        ")" to 1,
+        " )" to 2,
+        "()" to 0,
+        "())" to 3,
+        "*" to 0,
+        "(())" to 0,
+        "()()" to 0,
+        "(((" to 0,
+        "(()(()(" to 0,
+        "))(((((" to 1,
+        "())" to 3,
+        "))(" to 1,
+        ")))" to 1,
+        ")())())" to 1,
+        "()(()))" to 7,
+        "()()(()(()" to 0,
+        "(()(()))))" to 9,
+        "()()()()()()(" to 0,
+        "(()(()()()(())))" to 0,
+        "(()()()()(()(()(()))())())" to 0,
+        "(((())))((((()))))" to 0,
+        ")()()()()()()()()()" to 1,
+        "()(()(()))()())(((()))" to 15,
+        null to 0,
+        input to 1797,
+    )
+
     @Test
     fun `one move in elevator`() =
         oneMoveTestData.forEach { (instruction, direction) ->
-            move(instruction.toString()) shouldBe direction
+            move()(instruction.toString()) shouldBe direction
         }
 
     @Test
     fun `several moves in elevator`() =
         severalMovesTestData.forEach { (instructions, floor) ->
-            followInstructions(instructions.toString()) shouldBe floor
+            followInstructions()(instructions.toString()) shouldBe floor
         }
+
+    @Test
+    fun `find the basement`() =
+        findBasementTestData.forEach { (instructions, basement) ->
+            findBasementPosition(instructions.toString()) shouldBe basement
+    }
 }
